c6fd2d20dcc8c688b7b9f297f7c73f348bf11672
DJGosnell@gmail.com <DJGosnell@gmail.com@9741bb7c-c2c4-6f46-3ef1-f75609372c15>
2011-03-23 13:52:47 +0000

    Added missing files in last commit.

---
+++ trunk/dtxUploadClient/Forms/frmManage.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace dtxUpload {
+	partial class frmManage {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmManage));
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this._treFolders = new System.Windows.Forms.TreeView();
+			this._imlFolders = new System.Windows.Forms.ImageList(this.components);
+			this._lstFiles = new System.Windows.Forms.ListView();
+			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.mainMenu1 = new System.Windows.Forms.MainMenu(this.components);
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
+			this.menuItem17 = new System.Windows.Forms.MenuItem();
+			this.menuItem7 = new System.Windows.Forms.MenuItem();
+			this.menuItem8 = new System.Windows.Forms.MenuItem();
+			this.menuItem9 = new System.Windows.Forms.MenuItem();
+			this.menuItem3 = new System.Windows.Forms.MenuItem();
+			this.menuItem2 = new System.Windows.Forms.MenuItem();
+			this.menuItem5 = new System.Windows.Forms.MenuItem();
+			this.menuItem4 = new System.Windows.Forms.MenuItem();
+			this._imlFiles = new System.Windows.Forms.ImageList(this.components);
+			this._contextMenuFolders = new System.Windows.Forms.ContextMenu();
+			this.menuItem16 = new System.Windows.Forms.MenuItem();
+			this.menuItem10 = new System.Windows.Forms.MenuItem();
+			this.menuItem11 = new System.Windows.Forms.MenuItem();
+			this.menuItem12 = new System.Windows.Forms.MenuItem();
+			this.menuItem13 = new System.Windows.Forms.MenuItem();
+			this.menuItem14 = new System.Windows.Forms.MenuItem();
+			this.menuItem15 = new System.Windows.Forms.MenuItem();
+			this.vistaMenu = new dtxCore.Controls.VistaMenu(this.components);
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.vistaMenu)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Location = new System.Drawing.Point(0, 455);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(747, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this._treFolders);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this._lstFiles);
+			this.splitContainer1.Size = new System.Drawing.Size(747, 455);
+			this.splitContainer1.SplitterDistance = 198;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// _treFolders
+			// 
+			this._treFolders.Dock = System.Windows.Forms.DockStyle.Fill;
+			this._treFolders.ImageIndex = 0;
+			this._treFolders.ImageList = this._imlFolders;
+			this._treFolders.Location = new System.Drawing.Point(0, 0);
+			this._treFolders.Name = "_treFolders";
+			this._treFolders.SelectedImageIndex = 0;
+			this._treFolders.Size = new System.Drawing.Size(198, 455);
+			this._treFolders.TabIndex = 0;
+			this._treFolders.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this._treFolders_NodeMouseClick);
+			// 
+			// _imlFolders
+			// 
+			this._imlFolders.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
+			this._imlFolders.ImageSize = new System.Drawing.Size(16, 16);
+			this._imlFolders.TransparentColor = System.Drawing.Color.Transparent;
+			// 
+			// _lstFiles
+			// 
+			this._lstFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+			this._lstFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+			this._lstFiles.Location = new System.Drawing.Point(0, 0);
+			this._lstFiles.Name = "_lstFiles";
+			this._lstFiles.Size = new System.Drawing.Size(545, 455);
+			this._lstFiles.SmallImageList = this._imlFiles;
+			this._lstFiles.TabIndex = 0;
+			this._lstFiles.UseCompatibleStateImageBehavior = false;
+			this._lstFiles.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Name";
+			this.columnHeader1.Width = 219;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Size";
+			this.columnHeader2.Width = 78;
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Type";
+			this.columnHeader3.Width = 87;
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "DateUploaded";
+			this.columnHeader4.Width = 109;
+			// 
+			// columnHeader5
+			// 
+			this.columnHeader5.Text = "Views";
+			this.columnHeader5.Width = 47;
+			// 
+			// mainMenu1
+			// 
+			this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem1,
+            this.menuItem2});
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 0;
+			this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem17,
+            this.menuItem7,
+            this.menuItem8,
+            this.menuItem9,
+            this.menuItem3});
+			this.menuItem1.Text = "File";
+			// 
+			// menuItem17
+			// 
+			this.menuItem17.Index = 0;
+			this.menuItem17.Text = "New Folder";
+			// 
+			// menuItem7
+			// 
+			this.menuItem7.Index = 1;
+			this.menuItem7.Text = "Open Selection";
+			// 
+			// menuItem8
+			// 
+			this.menuItem8.Index = 2;
+			this.menuItem8.Text = "Copy Selection";
+			// 
+			// menuItem9
+			// 
+			this.menuItem9.Index = 3;
+			this.menuItem9.Text = "-";
+			// 
+			// menuItem3
+			// 
+			this.menuItem3.Index = 4;
+			this.menuItem3.Text = "Close";
+			// 
+			// menuItem2
+			// 
+			this.menuItem2.Index = 1;
+			this.menuItem2.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem5,
+            this.menuItem4});
+			this.menuItem2.Text = "Edit";
+			// 
+			// menuItem5
+			// 
+			this.menuItem5.Index = 0;
+			this.menuItem5.Text = "Move";
+			// 
+			// menuItem4
+			// 
+			this.menuItem4.Index = 1;
+			this.menuItem4.Text = "Delete";
+			// 
+			// _imlFiles
+			// 
+			this._imlFiles.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
+			this._imlFiles.ImageSize = new System.Drawing.Size(16, 16);
+			this._imlFiles.TransparentColor = System.Drawing.Color.Transparent;
+			// 
+			// _contextMenuFolders
+			// 
+			this._contextMenuFolders.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+            this.menuItem16,
+            this.menuItem10,
+            this.menuItem11,
+            this.menuItem12,
+            this.menuItem13,
+            this.menuItem14,
+            this.menuItem15});
+			// 
+			// menuItem16
+			// 
+			this.menuItem16.Index = 0;
+			this.menuItem16.Text = "New Folder";
+			// 
+			// menuItem10
+			// 
+			this.menuItem10.Index = 1;
+			this.menuItem10.Text = "Delete";
+			// 
+			// menuItem11
+			// 
+			this.menuItem11.Index = 2;
+			this.menuItem11.Text = "Rename";
+			// 
+			// menuItem12
+			// 
+			this.menuItem12.Index = 3;
+			this.menuItem12.Text = "Make Private";
+			// 
+			// menuItem13
+			// 
+			this.menuItem13.Index = 4;
+			this.menuItem13.Text = "Make Public";
+			this.menuItem13.Visible = false;
+			// 
+			// menuItem14
+			// 
+			this.menuItem14.Index = 5;
+			this.menuItem14.Text = "Share Folder";
+			// 
+			// menuItem15
+			// 
+			this.menuItem15.Index = 6;
+			this.menuItem15.Text = "Remove Folder Share";
+			this.menuItem15.Visible = false;
+			// 
+			// vistaMenu
+			// 
+			this.vistaMenu.ContainerControl = this;
+			// 
+			// frmManage
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(747, 477);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.statusStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Menu = this.mainMenu1;
+			this.Name = "frmManage";
+			this.Text = "Manage Uploaded Files";
+			this.Load += new System.EventHandler(this.frmManage_Load);
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.vistaMenu)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TreeView _treFolders;
+		private System.Windows.Forms.ListView _lstFiles;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.MainMenu mainMenu1;
+		private System.Windows.Forms.MenuItem menuItem1;
+		private System.Windows.Forms.MenuItem menuItem7;
+		private System.Windows.Forms.MenuItem menuItem8;
+		private System.Windows.Forms.MenuItem menuItem9;
+		private System.Windows.Forms.MenuItem menuItem3;
+		private System.Windows.Forms.MenuItem menuItem2;
+		private System.Windows.Forms.MenuItem menuItem5;
+		private System.Windows.Forms.MenuItem menuItem4;
+		private System.Windows.Forms.ColumnHeader columnHeader5;
+		private System.Windows.Forms.ImageList _imlFolders;
+		private System.Windows.Forms.ImageList _imlFiles;
+		private System.Windows.Forms.ContextMenu _contextMenuFolders;
+		private System.Windows.Forms.MenuItem menuItem17;
+		private System.Windows.Forms.MenuItem menuItem16;
+		private System.Windows.Forms.MenuItem menuItem10;
+		private System.Windows.Forms.MenuItem menuItem11;
+		private System.Windows.Forms.MenuItem menuItem12;
+		private System.Windows.Forms.MenuItem menuItem13;
+		private System.Windows.Forms.MenuItem menuItem14;
+		private System.Windows.Forms.MenuItem menuItem15;
+		private dtxCore.Controls.VistaMenu vistaMenu;
+
+	}
+}
---
+++ trunk/dtxUploadClient/Forms/frmManage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dtxUpload {
+	public partial class frmManage : Form {
+		ServerConnector connector = new ServerConnector();
+
+		public frmManage() {
+			Client.form_Manage = this;
+			InitializeComponent();
+		}
+
+		TreeNode node_root;
+
+		private void frmManage_Load(object sender, EventArgs e) {
+			_treFolders.ContextMenu = _contextMenuFolders;
+
+			string server_name = Client.server_info.server_name;
+			_imlFolders.Images.Add("folder", Properties.Resources.asset_blue_16_ns);
+			_imlFolders.Images.Add("folder_private", Properties.Resources.asset_grey_16_ns);
+			_imlFolders.Images.Add("server", Properties.Resources.circle_blue_16_ns);
+
+			_imlFiles.Images.Add("file", Properties.Resources.square_green_16_ns);
+			
+
+			if (server_name == "") {
+				server_name = "Server";
+			}
+			node_root = _treFolders.Nodes.Add("root", server_name, "server", "server");
+
+			node_root.Nodes.Add("folder_unsorted", "Unsorted Uploads", "folder", "folder");
+			node_root.Nodes.Add("folder_private", "Private", "folder_private", "folder_private");
+			node_root.Expand();
+		}
+
+		private void _treFolders_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e) {
+			if (e.Node.Name == "folder_unsorted") {
+				connector.callServerMethod("files_in_directory", "/");
+			}
+		}
+
+		public void displayFolderContents(DC_FileInformation[] files) {
+			foreach(DC_FileInformation file in files){
+				ListViewItem item = _lstFiles.Items.Add("file_" + file.url_id, file.file_name, "file");
+
+				item.SubItems.Add(file.file_size.ToString());
+				item.SubItems.Add("");
+				item.SubItems.Add(file.upload_date);
+			}
+		}
+	}
+}
