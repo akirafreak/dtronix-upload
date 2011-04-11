931fc606d8dae0ae8f6952b4d5438c339236f9b5
DJGosnell@gmail.com <DJGosnell@gmail.com@9741bb7c-c2c4-6f46-3ef1-f75609372c15>
2011-04-11 01:54:07 +0000

    Initial import of server packing program.

---
+++ trunk/dtxPhPacker/Properties/BuildInformation.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+// Major.Minor.Build(Total days since start of project)
+// Project begin date: Wednesday, February 09, 2011 1:04:00 AM
+[assembly: AssemblyVersion("0.2.60")]
+[assembly: AssemblyFileVersion("0.2.60")]
+[assembly: AssemblyCompany("Dtronix")]
+[assembly: AssemblyCopyright("©2011 Dtronix")]
